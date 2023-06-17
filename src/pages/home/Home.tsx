d8036755b4b81e2b23689f90c80ec218cc89bbe2
Mario Petkovic <mpetkovic.ri@gmail.com>
2023-06-17 10:19:23 +0200

    localization added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,14 @@
 import React, { useMemo } from 'react';
 import Col from 'antd/lib/col';
 import Row from 'antd/lib/row';
-import { Grid } from 'antd';
+import Grid from 'antd/lib/grid';
+import Button from 'antd/lib/button';
+import { useTranslation } from 'react-i18next';
 const { useBreakpoint } = Grid;
 
 export const Home = () => {
   const screens = useBreakpoint();
+  const { t } = useTranslation();
 
   const mobileScreen: boolean = useMemo(() => !!screens.xs, [screens]);
 
@@ -15,6 +18,14 @@ export const Home = () => {
         <Col span={mobileScreen ? 24 : 8}>One of three columns</Col>
         <Col span={mobileScreen ? 24 : 8}>One of three columns</Col>
         <Col span={mobileScreen ? 24 : 8}>One of three columns</Col>
+      </Row>
+      <Row>
+        <Col
+          span={24}
+          style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 32 }}
+        >
+          <Button size='large'>{t('res_submit')}</Button>
+        </Col>
       </Row>
     </>
   );
